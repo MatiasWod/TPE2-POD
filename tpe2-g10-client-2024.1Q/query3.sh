5f6c25afd499d051f94ebc8bc49e162eac5ff0da
tataabancens <tataabancens@gmail.com>
2024-06-12 23:25:09 -0300

    chore: cleaning workspace

--- tpe2-g10-client-2024.1Q/query3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PATH_TO_CODE_BASE=`pwd`
-
-#JAVA_OPTS="-Djava.rmi.server.codebase=file://$PATH_TO_CODE_BASE/lib/jars/rmi-params-client-1.0-SNAPSHOT.jar"
-
-MAIN_CLASS="ar.edu.itba.pod.queries.query3.Query3"
-
-java -DtimeLogFile="time3.txt" $* -cp 'lib/jars/*' $MAIN_CLASS
