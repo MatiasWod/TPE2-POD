3796a1cd8c8c67687eaad660e7aad415da604588
Alberto <tataabancens@gmail.com>
2024-06-13 23:21:04 -0300

    Remove unused file

--- unzip-tar.sh
+++
@@ -1,5 +0,0 @@
-tar -xf ./server/target/tpe2-g10-server-2024.1Q-bin.tar.gz
-tar -xf ./client/target/tpe2-g10-client-2024.1Q-bin.tar.gz
-
-chmod u+x ./tpe2-g10-client-2024.1Q/* ./tpe2-g10-server-2024.1Q/run-server.sh
-
